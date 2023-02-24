6eede07b20037bc5f77255462f62d781328406cc
Dhanush Tipparaju <dhanushtips@gmail.com>
2023-02-24 09:33:21 -0600

    Initial Commit

---
+++ Cats as as service API Calling/Cats_as_as_service_API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cats_as_as_service_API_CallingApp.swift
+//  Cats as as service API Calling
+//
+//  Created by Dhanush Tipparaju on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Cats_as_as_service_API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
